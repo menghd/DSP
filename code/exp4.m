4213cdee4164f426f63a661202cc924638770116
menghd <souray@qq.com>
2020-11-10 13:39:32 +0800

    fix

--- code/exp4.m
+++ code/exp4.m
@@ -2,7 +2,7 @@
 fp = 1000; fst = 4000;
 Rp = 3; As = 15;
 fs = 20000;
-[n, _] = buttord(2*pi*fp, 2*pi*fst, Rp, As, 's');
+[n, other] = buttord(2*pi*fp, 2*pi*fst, Rp, As, 's'); % 超出归一化1需要添加's'
 [z, p, k] = buttap(n);
 [b, a] = zp2tf(z, p, k);
 [bt, at] = lp2lp(b, a); % 连续滤波器
@@ -11,23 +11,25 @@ freqz(bz, az) % 频率响应
 
 % 2信号过滤
 fs = 20000; f1 = 20; f2 = 5000;
-T = 1; t = 0:1/fs:T/2; f = t;
+T = 1; t = 0:1/fs:T; f = 0:1/T:fs;
 x = sin(2*pi*f1*t) + sin(2*pi*f2*t);
 y = filter(bz, az, x);
 Wx = fft(x);
 Wy = fft(y);
 subplot(121)
-plot(t, x, 'r', t, y, 'g')
+plot(t, x, 'r', t, y, 'b')
 subplot(122)
-plot(f, Wx, 'r', f, Wy, 'g')
+plot(f, abs(Wx), 'r', f, abs(Wy), 'b')
 
 % 3加窗函数
 fp = 50; fst = 100; fs = 400;
 As = 40; % 查表选用Hanning窗
-deltaw = 2*pi*(fst - fp);
+deltaw = 2*pi*(fst - fp)/fs;
 n = ceil(6.2*pi/deltaw);
 h = hanning(n);
-f = fst + fp;
-figure = fir1(n - 1, f, h)
-stem(figure)
-freqz(figure, 1)
+f = (fst + fp)/fs;
+b = fir1(n - 1, f, h)
+figure
+stem(b)
+figure
+freqz(b, 1)
